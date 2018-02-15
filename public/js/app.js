0d0f7e55078157e090426ec24dabaec64229286c
Nefelijm <sistemasnik20@gmail.com>
2018-02-15 11:31:55 -0500

    linkedin Utilizando la data

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,29 @@
-window.addEventListener('load', function () {
-  // Setup an event listener to make an API call once auth is complete
-  IN.Event.on(IN, 'auth', getProfileData);
-  // Handle the successful return from the API call
-  function onSuccess(data) {
-    console.log(data);
-    
-  }
-  // manejando los errores
-  function onError(error) {
-    console.log(error);
-  }
-  // Use the API call wrapper to request the member's basic profile data
-  function getProfileData() {
-    IN.API.Raw('/people/~').result(onSuccess).error(onError);
-  }
+// Setup an event listener to make an API call once auth is complete
+function onLinkedInLoad() {
+  IN.Event.on(IN, "auth", getProfileData);
+}
 
-});
+// Handle the successful return from the API call
+function onSuccess(data) {
+  console.log(data);
+  localStorage.name = data.firstName;
+  console.log(localStorage.name)
+}
+
+// Handle an error response from the API call
+function onError(error) {
+  console.log(error);
+}
+
+function image(data){
+  localStorage.photo = data.pictureUrl
+  console.log(localStorage.photo);
+  window.location.href = '../views/profile.html';
+
+}
+
+// Use the API call wrapper to request the member's basic profile data
+function getProfileData() {
+  IN.API.Raw("/people/~").result(onSuccess).error(onError);
+  IN.API.Raw('people/~:(id,num-connections,picture-url)').result(image).error(onError);
+}
