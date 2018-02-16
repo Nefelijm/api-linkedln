2930e1bb38afa38ccbb6276a4a36079a4b73725d
Nefelijm <sistemasnik20@gmail.com>
2018-02-16 07:28:03 -0500

    resolviendo conflictos

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,2 +1,13 @@
+
+
 let name = document.getElementById('name');
 name.innerText = localStorage.name;
+
+let img = document.getElementById('image');
+img.setAttribute('src', localStorage.photo );
+
+let lastname = document.getElementById('lastname');
+lastname.innerText = localStorage.lastname;
+
+let description = document.getElementById('description');
+description.innerText = localStorage.description;
